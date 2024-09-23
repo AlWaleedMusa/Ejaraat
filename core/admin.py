442cb9f539c5ae86eb08600ad44f553c9bee666d
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-09-23 21:38:21 +0300

    add: tenant to the admin dashboard

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Property, RentProperty
-
+from .models import Property, RentProperty, Tenant
 
 admin.site.register(Property)
 admin.site.register(RentProperty)
-
+admin.site.register(Tenant)
