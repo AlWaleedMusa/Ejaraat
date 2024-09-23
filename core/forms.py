c91df2f877b91643ce44bac3d6c243ad4f246e7d
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-09-23 21:46:45 +0300

    update: labels translation added to forms

--- core/forms.py
+++ core/forms.py
@@ -21,8 +21,24 @@ class RentPropertyForm(forms.ModelForm):
 
     class Meta:
         model = RentProperty
-        exclude = ["property", "owner"]
+        exclude = ["property", "tenant"]
         widgets = {
             "start_date": DatePickerInput(options={"format": "DD/MM/YYYY"}),
             "end_date": DatePickerInput(options={"format": "DD/MM/YYYY"}),
         }
+        labels = {
+            'payment': _("Rent Payment Type"),
+            'down_payment': _("Down Payment"),
+            'start_date': _("Start Rent"),
+            'end_date': _("End Rent"),
+            'contract': _("Contract Image"),
+            'damage_deposit': _("Damage Deposit"),
+        }
+
+    def __init__(self, *args, **kwargs):
+        property = kwargs.pop("property", None)
+
+        super().__init__(*args, **kwargs)
+
+        if property:
+            self.fields['damage_deposit'].initial = property.price
