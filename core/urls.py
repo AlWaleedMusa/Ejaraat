5d286f4b9e5429cd125da0b8022852ea09a375a3
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-09-23 21:50:46 +0300

    add: view property, edit_property routs

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path("", views.landing, name="landing"),
     path("home/", views.home, name="home"),
     path("add/", views.add_property, name="add_property"),
-    path("rent/<int:pk>/", views.rent_property, name="rent_property"),
+    
+    path("rent_property/<int:pk>/", views.rent_property, name="rent_property"),
 
     path("all_properties/", views.all_properties, name="all_properties"),
     path("delete_property/<int:pk>", views.delete_property, name="delete_property"),
