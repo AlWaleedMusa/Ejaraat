23373914c85ccb2df6992610cab6eb122bc2ae62
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-10-09 23:03:22 +0300

    fix: get upcoming payment with advanced feature to set status for rentals

--- core/utils.py
+++ core/utils.py
@@ -8,13 +8,35 @@ def get_expiring_contracts(properties):
         if rental.expiring_contracts()
     ]
 
+def get_days_overdued(rental, next_payment):
+    """
+    """
+    year = rental.start_date.year
+    month = rental.start_date.month
+    days_in_month = calendar.monthrange(year, month)[1]
+    return days_in_month - next_payment
 
 def get_upcoming_payments(properties):
     """
     """
-    return [
-        rental
-        for property in properties
-        for rental in property.property_rentals.all()
-        if (next_payment := rental.get_next_payment()) and next_payment <= 7
-    ]
+    upcoming_payments = []
+
+    for property in properties:
+        for rental in property.property_rentals.all():
+            next_payment = rental.get_next_payment()
+            # if there is an expected payment
+            if next_payment is not None:
+                days_overdued = get_days_overdued(rental, next_payment)
+                if next_payment <= 7 :
+                    upcoming_payments.append(rental)
+                elif (days_overdued >= 0 and not date.today().day == rental.start_date.day) and not rental.status == "paid":
+                    rental.status = "overdue"
+                    rental.save()
+                    upcoming_payments.append(rental)
+            # check if the last month is paid or not
+            elif not rental.status == "paid":
+                rental.status = "overdue"
+                rental.save()
+                upcoming_payments.append(rental)
+
+    return upcoming_payments
