d7cbe7ed79086fe79ae6995b442ff119b7d26753
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-10-16 12:18:32 +0300

    update: code black

--- core/utils.py
+++ core/utils.py
@@ -5,8 +5,7 @@ from .models import Notifications, RecentActivity
 
 
 def get_expiring_contracts(properties):
-    """
-    """
+    """ """
     return [
         rental
         for property in properties
@@ -14,9 +13,9 @@ def get_expiring_contracts(properties):
         if rental.expiring_contracts()
     ]
 
+
 def get_upcoming_payments(properties):
-    """
-    """
+    """ """
     upcoming_payments = []
     today = date.today()
 
@@ -35,7 +34,7 @@ def get_upcoming_payments(properties):
                             rental.status = "overdue"
                             rental.save()
                         upcoming_payments.append(rental)
-                    else:                    
+                    else:
                         # Handle pending payments due within 7 days
                         if rental.status != "pending" and rental.status != "paid":
                             rental.status = "pending"
@@ -45,7 +44,7 @@ def get_upcoming_payments(properties):
                         # Always append rentals due within 7 days, regardless of status
                         if rental not in upcoming_payments or rental.status != "paid":
                             upcoming_payments.append(rental)
-                    
+
                     if rental.status == "paid" and rental in upcoming_payments:
                         upcoming_payments.remove(rental)
             else:
@@ -62,20 +61,21 @@ def get_upcoming_payments(properties):
 
 
 def get_recent_activity(user):
-    """
-    """
-    return RecentActivity.objects.filter(user=user).exclude(activity_type="overdue").order_by("-timestamp")[:10]
+    """ """
+    return (
+        RecentActivity.objects.filter(user=user)
+        .exclude(activity_type="overdue")
+        .order_by("-timestamp")[:10]
+    )
+
 
 def get_notifications(user):
-    """
-    """
+    """ """
     return Notifications.objects.filter(user=user, is_read=False)
 
 
-
 def convert_currency(amount, from_currency, to_currency="USD"):
-    """
-    """
+    """ """
     url = f"https://v6.exchangerate-api.com/v6/{os.getenv('CURRENCY_CONVERTER_API')}/latest/{from_currency}"
     response = requests.get(url)
     data = response.json()
@@ -89,18 +89,17 @@ def convert_currency(amount, from_currency, to_currency="USD"):
             raise ValueError(f"conversion rate for {to_currency} not found")
 
 
-def get_monthly_earning(properties):
-    """
-    """
+def get_monthly_revenue(properties):
+    """ """
     today = date.today()
     month = today.month
     year = today.year
-    monthly_earning = 0
+    monthly_revenue = 0
 
     for property in properties:
         for rental in property.property_rentals.all():
             if rental.start_date.month == month and rental.start_date.year == year:
-                converted_price = convert_currency(rental.price, rental.property.currency)
-                monthly_earning += converted_price
+                # converted_price = convert_currency(rental.price, rental.property.currency)
+                monthly_revenue += 1  # converted_price
 
-    return monthly_earning
+    return monthly_revenue
