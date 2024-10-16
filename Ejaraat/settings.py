580f8bf5fd64bbe7b23331185bb2122bdaaeefb6
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-10-16 04:18:35 +0300

    ft: email configurations ready and sending mails is working

--- Ejaraat/settings.py
+++ Ejaraat/settings.py
@@ -173,6 +173,15 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = f"Ejaraat {os.getenv('EMAIL_HOST_USER')}"
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
+
 
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
