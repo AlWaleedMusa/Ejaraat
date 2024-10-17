356922071774ca77e9226b0729a75e2a19a5762c
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-10-17 03:53:51 +0300

    refactor: utils.py and documenting function

--- core/utils.py
+++ core/utils.py
@@ -5,7 +5,18 @@ from .models import Notifications, RecentActivity
 
 
 def get_expiring_contracts(properties):
-    """ """
+    """
+    Retrieve a list of rentals with expiring contracts from a given list of properties.
+
+    This function iterates over a list of properties and their associated rentals to find
+    rentals with expiring contracts.
+
+    Args:
+        properties (QuerySet): A QuerySet of Property instances.
+
+    Returns:
+        list: A list of Rental instances with expiring contracts.
+    """
     return [
         rental
         for property in properties
@@ -15,7 +26,18 @@ def get_expiring_contracts(properties):
 
 
 def get_upcoming_payments(properties):
-    """ """
+    """
+    Retrieve a list of rentals with upcoming payments from a given list of properties.
+
+    This function iterates over a list of properties and their associated rentals to find
+    rentals with payments due within the next 7 days or overdue payments.
+
+    Args:
+        properties (QuerySet): A QuerySet of Property instances.
+
+    Returns:
+        list: A list of Rental instances with upcoming payments.
+    """
     upcoming_payments = []
     today = date.today()
 
@@ -60,29 +82,30 @@ def get_upcoming_payments(properties):
     return upcoming_payments
 
 
-def get_recent_activity(user):
-    """ """
-    return (
-        RecentActivity.objects.filter(user=user)
-        .exclude(activity_type="overdue")
-        .order_by("-timestamp")[:10]
-    )
-
-
-def get_notifications(user):
-    """ """
-    return Notifications.objects.filter(user=user, is_read=False)
-
-
 def convert_currency(amount, from_currency, to_currency="USD"):
-    """ """
+    """
+    Convert an amount from one currency to another using the exchange rate API.
+
+    This function fetches the latest exchange rates from the API and converts the given amount
+    from the source currency to the target currency.
+
+    Args:
+        amount (float): The amount to be converted.
+        from_currency (str): The source currency code.
+        to_currency (str, optional): The target currency code. Defaults to "USD".
+
+    Returns:
+        float: The converted amount in the target currency.
+
+    Raises:
+        ValueError: If the conversion rate for the target currency is not found.
+    """
     url = f"https://v6.exchangerate-api.com/v6/{os.getenv('CURRENCY_CONVERTER_API')}/latest/{from_currency}"
     response = requests.get(url)
     data = response.json()
 
     if response.status_code == 200:
         if to_currency in data["conversion_rates"]:
-            data = response.json()
             rate = data["conversion_rates"][to_currency]
             return amount * rate
         else:
@@ -90,16 +113,28 @@ def convert_currency(amount, from_currency, to_currency="USD"):
 
 
 def get_monthly_revenue(properties):
-    """ """
+    """
+    Calculate the total monthly revenue for a given list of properties.
+
+    This function iterates over a list of properties and their associated rentals to calculate
+    the total revenue generated for the current month. It sums up the rental prices for rentals
+    that are active during the current date.
+
+    Args:
+        properties (QuerySet): A QuerySet of Property instances.
+
+    Returns:
+        float: The total monthly revenue for the given properties.
+    """
     today = date.today()
-    month = today.month
-    year = today.year
     monthly_revenue = 0
 
     for property in properties:
         for rental in property.property_rentals.all():
-            if rental.start_date.month == month and rental.start_date.year == year:
-                # converted_price = convert_currency(rental.price, rental.property.currency)
-                monthly_revenue += 1  # converted_price
+            if rental.start_date <= today and rental.end_date >= today:
+                converted_price = convert_currency(
+                    rental.price, rental.property.currency
+                )
+                monthly_revenue += converted_price
 
     return monthly_revenue
