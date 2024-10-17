0fe2d4fb3dabf90904ed80dee4f1fb5bb9ee8fe4
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-10-17 03:53:33 +0300

    refactor: signal.py and documenting function

--- core/signals.py
+++ core/signals.py
@@ -1,49 +1,85 @@
+from asgiref.sync import async_to_sync
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Property, RecentActivity, RentProperty, Notifications
 from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
-from datetime import datetime
+
+from .models import Property, RecentActivity, RentProperty, Notifications
 
 
 @receiver(post_save, sender=Property)
-def create_rent_history(sender, instance, created, **kwargs):
-    if created:
+@receiver(post_save, sender=RentProperty)
+def create_recent_activity(sender, instance, created, **kwargs):
+    """
+    Signal receiver that creates recent activity records for Property and RentProperty instances.
+
+    This function is triggered whenever a Property or RentProperty instance is saved. It creates
+    recent activity records based on the type of instance and its status.
+
+    Args:
+        sender (Model): The model class that sent the signal.
+        instance (Model instance): The instance of the model that was saved.
+        created (bool): A boolean indicating whether the instance was created.
+        **kwargs: Additional keyword arguments.
+
+    Actions:
+        - Creates a recent activity record when a Property instance is created.
+        - Creates a recent activity record when a RentProperty instance is created.
+        - Creates a recent activity record when a RentProperty instance's status is updated to "paid".
+    """
+
+    # Create recent activity for Property instance
+    if sender == Property and created:
         RecentActivity.objects.create(
             user=instance.user,
             property=instance,
             activity_type="add",
         )
-
-
-@receiver(post_save, sender=RentProperty)
-def create_rent_history(sender, instance, created, **kwargs):
-    if created:
-        RecentActivity.objects.create(
-            user=instance.property.user,
-            property=instance.property,
-            activity_type="rent",
-        )
-    else:
-        if instance.status == "paid":
+    # Create recent activity for RentProperty instance
+    elif sender == RentProperty:
+        if created:
             RecentActivity.objects.create(
                 user=instance.property.user,
                 property=instance.property,
-                activity_type="payment",
+                activity_type="rent",
             )
-        if instance.status == "overdue":
-            RecentActivity.objects.create(
-                user=instance.property.user,
-                property=instance.property,
-                activity_type="overdue",
-            )
+        else:
+            if instance.status == "paid":
+                RecentActivity.objects.create(
+                    user=instance.property.user,
+                    property=instance.property,
+                    activity_type="payment",
+                )
+            elif instance.status == "overdue":
+                RecentActivity.objects.create(
+                    user=instance.property.user,
+                    property=instance.property,
+                    activity_type="overdue",
+                )
 
 
 @receiver(post_save, sender=RecentActivity)
-def send_recent_activities(sender, instance, created, **kwargs):
+def handle_recent_activity(sender, instance, created, **kwargs):
+    """
+    Signal receiver that handles recent activities and overdue notifications when a RecentActivity instance is created.
+
+    This function is triggered whenever a RecentActivity instance is saved. If the instance is newly created,
+    it handles both recent activities and overdue notifications based on the activity_type.
+
+    Args:
+        sender (Model): The model class that sent the signal.
+        instance (RecentActivity): The instance of the model that was saved.
+        created (bool): A boolean indicating whether the instance was created.
+        **kwargs: Additional keyword arguments.
+
+    Actions:
+        - Handles recent activities if the activity_type is not "overdue".
+        - Creates a notification for the user if the activity_type is "overdue".
+        - Sends the appropriate data to the user's WebSocket channel.
+    """
     if created:
         channel_layer = get_channel_layer()
 
+        # Handle recent activities and overdue notifications
         if instance.activity_type != "overdue":
             recent_activities = list(
                 RecentActivity.objects.filter(user=instance.user)
@@ -57,14 +93,7 @@ def send_recent_activities(sender, instance, created, **kwargs):
                     "recent_activities": recent_activities,
                 },
             )
-
-
-@receiver(post_save, sender=RecentActivity)
-def send_overdue_notifications(sender, instance, created, **kwargs):
-    if created:
-        channel_layer = get_channel_layer()
-
-        if instance.activity_type == "overdue":
+        else:
             Notifications.objects.create(
                 user=instance.user,
                 property=instance.property,
